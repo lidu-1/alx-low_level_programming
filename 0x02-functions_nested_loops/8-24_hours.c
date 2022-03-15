57c38a73b8880c9206e4ff06087720caa3af312f
lidiya yohannes <lilujoli65@gmail.com>
2022-03-15 12:29:44 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,22 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-  * jack_bauer - Prints every minute of the day of Jack Bauer
-  *
-  * Return: ...
-  */
+ * jack_bauer - prints every minute of the day 00:00 to 23:59
+ * @void - no argument
+ */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int i, j;
 
-	for (a = 0; a <= 2; a++)
+	for (i = 0; i < 24; i++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (j = 0; j < 60; j++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
