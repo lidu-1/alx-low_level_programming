f42edf2361d0f97661bf5f437d9af65b0d341ab9
lidiya yohannes <lilujoli65@gmail.com>
2022-03-15 05:54:41 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,18 @@
-#include "holberton.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-  * print_alphabet_x10 - Make alphabet x10 times
-  *
-  * Return: void
-  */
-void print_alphabet_x10(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char c;
-	int i = 0;
+	char low;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 
-		_putchar('\n');
-
-		i++;
-	}
+	return (0);
 }
