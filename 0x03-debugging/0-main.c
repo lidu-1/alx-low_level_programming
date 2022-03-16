96f40af437df162f3f3737b23c7030fccb7d725d
lidiya yohannes <lilujoli65@gmail.com>
2022-03-16 12:48:38 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - tests func that prints if an integer is + or - or zero
- * Return: 0
+ *main - Debugging.
+ *
+ *Return: 0
  */
 
 int main(void)
 {
-	int i;
+  int i;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+  i = 0;
+  positive_or_negative(i);
+  return (0);
 }
