11931da31f72b13b4fc9e7ff801efeae83adfff9
lidiya yohannes <lilujoli65@gmail.com>
2022-04-13 12:44:52 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
-
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
-int main(int argc, char *argv[])
+ * main - prints the opcodes of its own main function.
+ * @argc: number of arguments
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char **argv)
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int con, byte;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return (1);
 	}
-
+	byte = atoi(argv[1]);
+	if (byte < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (con = 0; con < (byte - 1); con++)
+	{
+		if (byte != 0)
+			printf("%02hhx ", ((char *)main)[con]);
+	}
+	printf("%02hhx\n", ((char *)main)[con]);
 	return (0);
 }
