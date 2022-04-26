e0cbf4a602b2d0c2a3e2eb2471554ecbf31b07c4
lidiya yohannes <lilujoli65@gmail.com>
2022-04-26 13:11:24 -0700

    list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,19 @@
-#ifndef LISTS_H_
-#define LISTS_H_
-
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
 #include <string.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
